45514766f81bc4a88f6f46f5c3cf9bd01ce4e597
Gessio Mori <gessiomori@protonmail.com>
2023-01-19 15:11:59 -0300

    backend: implements account module tests

---
+++ server/src/tests/account/controllers/createAccount.controller.test.ts
@@ -0,0 +1,38 @@
+import 'reflect-metadata';
+import { connect, connection, set } from 'mongoose';
+import { config } from 'dotenv';
+import request from 'supertest';
+import { app } from '../../../infra/http/app';
+
+describe('Create Account Controller', () => {
+  beforeEach(async () => {
+    config();
+    set('strictQuery', true);
+    await connect(process.env.MONGO_TEST_URI || '');
+    const collections = await connection.db.collections();
+    for (const collection of collections) {
+      await collection.deleteMany({});
+    }
+  });
+  afterEach(async () => {
+    await connection.close();
+  });
+  it('Should be able to create a new account', async () => {
+    const response = await request(app).post('/account/signup').send({
+      username: 'tester',
+      password: 'tester',
+    });
+    expect(response.status).toBe(201);
+  });
+  it('Should not be able to create a new account with an existing username', async () => {
+    await request(app).post('/account/signup').send({
+      username: 'tester',
+      password: 'tester',
+    });
+    const response = await request(app).post('/account/signup').send({
+      username: 'tester',
+      password: 'tester',
+    });
+    expect(response.status).toBe(409);
+  });
+});
