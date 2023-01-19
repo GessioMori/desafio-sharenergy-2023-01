5fe34af2533cdf61dbabe2703beed49c61f00c56
Gessio Mori <gessiomori@protonmail.com>
2023-01-19 15:30:24 -0300

    backend: implements api docs

--- server/src/infra/http/app.ts
+++ server/src/infra/http/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import swagger from 'swagger-ui-express';
 import { config } from 'dotenv';
 
 import { ErrorHandler } from './errors/ErrorHandler';
@@ -10,6 +11,7 @@ import { accountRouter } from './routes/account.router';
 import { clientRouter } from './routes/client.router';
 
 import '../../containers/index';
+import swaggerFile from './../../docs/swagger.json';
 
 config();
 
@@ -19,9 +21,10 @@ app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+if (process.env.ENVIRONMENT !== 'test') {
+  app.use('/api-docs', swagger.serve, swagger.setup(swaggerFile));
+}
+
 app.use('/account', accountRouter);
 app.use('/client', clientRouter);
 app.use(ErrorHandler);
